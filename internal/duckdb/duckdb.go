922c4c97f976bbf0b87c1dce3c0157f468b4bdce
Nils Jacobi <76872969+WhisperN@users.noreply.github.com>
2025-08-02 11:56:40 +0200

    DuckDB up and running with imports

--- internal/duckdb/duckdb.go
+++ internal/duckdb/duckdb.go
@@ -1,132 +1,186 @@
-// sources:
-// 1. https://github.com/PacktPublishing/In-Memory-Analytics-with-Apache-Arrow-Second-Edition/blob/main/chapter8/go/main.go
-
-package duckdb
+package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 
 	"github.com/apache/arrow-adbc/go/adbc"
 	"github.com/apache/arrow-adbc/go/adbc/drivermgr"
-	"github.com/apache/arrow/go/v18/arrow"
-	"github.com/apache/arrow/go/v18/arrow/memory"
-	"github.com/apache/arrow/go/v18/parquet/file"
-	"github.com/apache/arrow/go/v18/parquet/pqarrow"
+	"github.com/apache/arrow-go/v18/arrow"
+	//"github.com/apache/arrow-go/v18/arrow/array"
+	"github.com/apache/arrow-go/v18/arrow/ipc"
+	//"github.com/apache/arrow-go/v18/arrow/memory"
 )
 
-func stdInterface() adbc.Database {
-	var duckDriver drivermgr.Driver
-	db, err := duckDriver.NewDatabase(map[string]string{
+const tableName string = "sPlot"
+
+type DuckDBSQLRunner struct {
+	ctx  context.Context
+	conn adbc.Connection
+	db   adbc.Database
+}
+
+func NewDuckDBSQLRunner(ctx context.Context) (*DuckDBSQLRunner, error) {
+	var drv drivermgr.Driver
+	db, err := drv.NewDatabase(map[string]string{
 		"driver":     "duckdb",
 		"entrypoint": "duckdb_adbc_init",
 		"path":       ":memory:",
 	})
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("failed to create new in-memory DuckDB database: %w", err)
 	}
-	defer db.Close()
-
-	return db
+	conn, err := db.Open(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open connection to new in-memory DuckDB database: %w", err)
+	}
+	return &DuckDBSQLRunner{ctx: ctx, conn: conn, db: db}, err
 }
 
-func populateDuckDB(db adbc.Database) {
-	ctx := context.Background()
-
-	rdr, err := file.OpenParquetFile("../../data/sPlot_CWM_CWV.parquet", false)
-	if err != nil {
-		panic(err)
+/*
+ * Record serializer
+ * Returns buffer io.Reader and error
+ */
+func serializeRecord(record arrow.Record) (io.Reader, error) {
+	// Allocates new dynamic memory
+	buf := new(bytes.Buffer)
+	wr := ipc.NewWriter(buf, ipc.WithSchema(record.Schema()))
+	if err := wr.Write(record); err != nil {
+		return nil, fmt.Errorf("failed to write record: %w", err)
 	}
-	defer rdr.Close()
-
-	pqrdr, err := pqarrow.NewFileReader(rdr,
-		pqarrow.ArrowReadProperties{
-			Parallel:  true,
-			BatchSize: 102400},
-		memory.DefaultAllocator)
-	if err != nil {
-		panic(err)
+	if err := wr.Close(); err != nil {
+		return nil, fmt.Errorf("failed to close writer: %w", err)
 	}
-
-	// Filter out arrow.NULL columns
-	/* I think we can avoid this if we filter beforehand?
-	 * Currently it's just a failsafe statement */
-	colIndices := make([]int, 0)
-	for _, f := range pqrdr.Manifest.Fields {
-		if f.Field.Type.ID() != arrow.NULL {
-			colIndices = append(colIndices, f.ColIndex)
-		}
-	}
-
-	recrdr, err := pqrdr.GetRecordReader(ctx, colIndices, nil)
-	if err != nil {
-		panic(err)
-	}
-	defer recrdr.Release()
-	/* End failsafe */
-	/*
-		cnxn, err := db.Open(ctx)
-		if err != nil {
-			panic(err)
-		}
-
-		stmt, err := cnxn.NewStatement()
-		if err != nil {
-			panic(err)
-		}
-		defer stmt.Close()
-
-		stmt.SetOption(adbc.OptionKeyIngestMode, adbc.OptionValueIngestModeReplace)
-		stmt.SetOption(adbc.OptionKeyIngestTargetTable, "")
-
-		if err := stmt.BindStream(ctx, recrdr); err != nil {
-			panic(err)
-		}
-
-		n, err := stmt.ExecuteUpdate(ctx)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println(n)*/
+	return buf, nil
 }
 
-func sqlInterface(db adbc.Database) {
-	ctx := context.Background()
-	cnxn, err := db.Open(ctx)
+/*
+ * Record importer
+ * @param sr io.Reader: Native object in Go for Buffer that data gets written to
+ * @param tableName string: The table name we want to write the sr to.
+ * Returns void
+ */
+func (r *DuckDBSQLRunner) importRecord(sr io.Reader) error {
+	rdr, err := ipc.NewReader(sr)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to create IPC reader: %w", err)
 	}
-	defer cnxn.Close()
-
-	sc, err := cnxn.GetTableSchema(ctx, nil, nil, "foo")
+	defer rdr.Release()
+	stmt, err := r.conn.NewStatement()
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to create new statement: %w", err)
 	}
-	fmt.Println(sc)
+	if err := stmt.SetOption(adbc.OptionKeyIngestMode, adbc.OptionValueIngestModeCreate); err != nil {
+		return fmt.Errorf("failed to set ingest mode: %w", err)
+	}
+	if err := stmt.SetOption(adbc.OptionKeyIngestTargetTable, tableName); err != nil {
+		return fmt.Errorf("failed to set ingest target table: %w", err)
+	}
+	if err := stmt.BindStream(r.ctx, rdr); err != nil {
+		return fmt.Errorf("failed to bind stream: %w", err)
+	}
+	if _, err := stmt.ExecuteUpdate(r.ctx); err != nil {
+		return fmt.Errorf("failed to execute update: %w", err)
+	}
+	return stmt.Close()
+}
 
-	stmt, err := cnxn.NewStatement()
+/*
+ * Runs an SQL command on top of a database
+ * @param sql string: takes an SQL string
+ * returns array of type arrow.Record
+ */
+func (r *DuckDBSQLRunner) runSQL(sql string) ([]arrow.Record, error) {
+	stmt, err := r.conn.NewStatement()
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("failed to create new statement: %w", err)
 	}
 	defer stmt.Close()
 
-	stmt.SetSqlQuery("SHOW TABLES")
-	rdr, n, err := stmt.ExecuteQuery(ctx)
+	if err := stmt.SetSqlQuery(sql); err != nil {
+		return nil, fmt.Errorf("failed to set SQL query: %w", err)
+	}
+	out, n, err := stmt.ExecuteQuery(r.ctx)
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("failed to execute query: %w", err)
 	}
-	defer rdr.Release()
+	defer out.Release()
 
-	fmt.Println(n)
-
-	for rdr.Next() {
-		fmt.Println(rdr.Record())
+	result := make([]arrow.Record, 0, n)
+	for out.Next() {
+		rec := out.Record()
+		rec.Retain() // .Next() will release the record, so we need to retain it
+		result = append(result, rec)
 	}
+	if out.Err() != nil {
+		return nil, out.Err()
+	}
+	return result, nil
+}
+
+/*
+ * Runs SQL on a Table of the database
+ * @param record arrow.Record: Something that is currently in the Database
+ * @param sql string: The SQL query we want to run on the entry
+ */
+func (r *DuckDBSQLRunner) RunSQLOnRecord(record arrow.Record, sql string) ([]arrow.Record, error) {
+	serializedRecord, err := serializeRecord(record)
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialize record: %w", err)
+	}
+	if err := r.importRecord(serializedRecord); err != nil {
+		return nil, fmt.Errorf("failed to import record: %w", err)
+	}
+	result, err := r.runSQL(sql)
+	if err != nil {
+		return nil, fmt.Errorf("failed to run SQL: %w", err)
+	}
+
+	// DANGER ZONE: Sniff snoff...
+	/*
+		if _, err := r.runSQL("DROP TABLE temp_table"); err != nil {
+			return nil, fmt.Errorf("failed to drop temp table after running query: %w", err)
+		}*/
+	return result, nil
+}
+
+/*
+ * For winning the beauty contest...
+ * "Your boy is always crispy clean"
+ * ~Money Boy
+ */
+func (r *DuckDBSQLRunner) Close() {
+	r.conn.Close()
+	r.db.Close()
 }
 
 func main() {
-	var database = stdInterface()
-	// populateDuckDB(database)
-	sqlInterface(database)
+	ctx := context.Background()
+	var db *DuckDBSQLRunner
+	db, err := NewDuckDBSQLRunner(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	fmt.Println("DuckDBSQLRunner started")
+	/*
+	 * If you want to import something from a .parquet file then run it in an SQL Query
+	 * SELECT * FROM read_parquet('input.parquet');
+	 * find other examples here: https://duckdb.org/docs/stable/guides/file_formats/parquet_import
+	 */
+
+	// let's try this...
+	result, err := db.runSQL("SELECT * FROM read_parquet('third_party/dataset/sPlot_CWM_CWV.parquet')")
+	if err != nil {
+		panic(err)
+	}
+
+	// Works like a charme
+	fmt.Println("Executed sql Query")
+	for _, record := range result {
+		fmt.Println(record)
+		record.Release()
+	}
 }
--- internal/duckdb/go.mod
+++ internal/duckdb/go.mod
@@ -5,6 +5,7 @@ go 1.24.5
 require (
 	github.com/apache/arrow-adbc/go/adbc v1.7.0
 	github.com/apache/arrow/go/v18 v18.0.0-20241007013041-ab95a4d25142
+	github.com/marcboeker/go-duckdb v1.8.5
 )
 
 require (
@@ -12,9 +13,11 @@ require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/apache/arrow-go/v18 v18.3.1 // indirect
 	github.com/apache/thrift v0.21.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/flatbuffers v25.2.10+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/asmfmt v1.3.2 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
